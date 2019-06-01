ddf1ddcb6da0ccab561e431c7bf1b0340f32cc1b
LoeiFy <LoeiFy@gmail.com>
2019-06-01 21:34:16 +0800

    blog content

--- scripts/hook.js
+++ scripts/hook.js
@@ -1,2 +1,15 @@
+const { join } = require('path')
+
 module.exports = (acyort) => {
+  const { base, public: publicDir } = acyort.config.get()
+
+  acyort.hooks.tap('pigeon_after_fetch', (issues) => {
+    for (let i = 0; i < issues.length; i += 1) {
+      issues[i].milestone = { title: 'blog' }
+    }
+  })
+
+  acyort.hooks.tap('pigeon_after_render', () => {
+    acyort.fs.outputFileSync(join(base, publicDir, 'CNAME'), 'acyort.am0200.com')
+  })
 }
